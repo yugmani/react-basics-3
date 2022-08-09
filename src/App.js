dac7d4443d7abc819a24ea84ddef54fddd0ae552
yugmani <yugmani@yahoo.com>
2022-08-08 22:38:03 -0700

    react portal

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './style.css';
 import RefsDemo from './components/RefsDemo';
 import GetInput from './components/GetInput';
 import GetInputHook from './components/GetInputHook';
+import PortalDemo from './components/PortalDemo';
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
       <RefsDemo />
       <GetInput />
       <GetInputHook />
+      <PortalDemo />
     </div>
   );
 }
